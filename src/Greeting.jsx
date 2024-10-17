e231188ece5a3847747c005107eea5ac911aae66
otomdee <otomdaniel65@gmail.com>
2024-10-17 22:31:51 +0100

    edit greeting component

---
+++ src/Greeting.jsx
@@ -0,0 +1,15 @@
+export function Greeting() {
+
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <ul>
+                {animals.map((animal) => {
+                    return <li key={animal}>{animal}</li>;
+                })}
+            </ul>
+        </div>
+);
+}
